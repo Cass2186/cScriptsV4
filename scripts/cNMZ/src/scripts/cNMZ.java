97c57f3258498241eeb457ba493afe78fb9991b2
Cass2186 <Casstinlinegoodfellow@gmai.com>
2022-04-03 17:55:24 -0400

    NMZ main cleanup

--- scripts/cNMZ/src/scripts/cNMZ.java
+++ scripts/cNMZ/src/scripts/cNMZ.java
@@ -29,10 +29,9 @@ import java.util.concurrent.atomic.AtomicBoolean;
 
 
 @ScriptManifest(name = "cNMZ v2.1", authors = {"Cass2186"}, category = "Testing")
-public class cNMZ extends Script implements Starting, Arguments, Painting, Ending, MessageListening07 {
+public class cNMZ extends Script implements Starting, Ending, MessageListening07 {
 
     public static AtomicBoolean isRunning = new AtomicBoolean(true);
-    public static String status = "Initializing";
 
     public void populateInitialMap() {
         Log.log("[Debug]: Populating intial skills xp HashMap");
@@ -40,19 +39,6 @@ public class cNMZ extends Script implements Starting, Arguments, Painting, Endin
             Vars.get().skillStartXpMap.put(s, s.getXP());
         }
     }
-
-    public HashMap<Skills.SKILLS, Integer> getXpMap() {
-        HashMap<Skills.SKILLS, Integer> map = new HashMap<>();
-        for (Skills.SKILLS s : Skills.SKILLS.values()) {
-            int startXp = Vars.get().skillStartXpMap.get(s);
-            if (s.getXP() > startXp) {
-                map.put(s, s.getXP() - startXp);
-            }
-        }
-
-        return map;
-    }
-
 
     @Override
     public void run() {
@@ -83,93 +69,13 @@ public class cNMZ extends Script implements Starting, Arguments, Painting, Endin
 
             Task task = tasks.getValidTask();
             if (task != null) {
-                status = task.toString();
+                Vars.get().status = task.toString();
                 task.execute();
             }
         }
 
     }
 
-    @Override
-    public void passArguments(HashMap<String, String> hashMap) {
-        String scriptSelect = hashMap.get("custom_input");
-        String clientStarter = hashMap.get("autostart");
-        String input = clientStarter != null ? clientStarter : scriptSelect;
-        General.println("[Debug]: Argument entered: " + input);
-
-        for (String arg : input.split(";")) {
-            try {
-
-
-            } catch (Exception e) {
-                e.printStackTrace();
-                General.println("[Error]: Args are incorrect", Color.RED);
-            }
-        }
-    }
-
-
-    @Override
-    public void onPaint(Graphics g) {
-    /*       int currentLvl = Skills.getActualLevel(currentSkill);
-        gainedLvl = currentLvl - startLevel;
-        gainedXp = Skills.getXP(currentSkill) - startXp;
-        HashMap<Skills.SKILLS, Integer> xpMap = getXpMap();
-        double timeRan = getRunningTime();
-        double timeRanMin = (timeRan / 3600000);
-
-        List<String> myString = new ArrayList<>(Arrays.asList(
-                "cNMZ",
-                "Running For: " + Timing.msToString(getRunningTime()),
-                "Task: " + status,
-                "Drinking Absorption at: " + Vars.get().drinkAbsorptionAt,
-                "Eating rockcake at: " + Vars.get().eatRockCakeAt
-             //   "Vars.get().add:  " + Vars.get().add
-        ));
-        for (Skills.SKILLS s : xpMap.keySet()){
-           int startLvl =  Skills.getLevelByXP(Vars.get().skillStartXpMap.get(s));
-            int xpHr = (int) (xpMap.get(s) / timeRanMin);
-            long xpToLevel1 = Skills.getXPToLevel(s, s.getActualLevel() + 1);
-            long ttl = (long) ( xpToLevel1 / ((xpMap.get(s) / timeRan)));
-            int gained = s.getActualLevel() - startLvl;
-            if (gained >0) {
-                myString.add(StringUtils.capitalize(s.toString().toLowerCase(Locale.ROOT))
-                        + " [" + s.getActualLevel() + "(" + gained + ")]: "
-                        + Utils.addCommaToNum(xpMap.get(s)) + "xp (" + Utils.addCommaToNum(xpHr) + "/hr) " +
-                        "|| TNL: "
-                        + Timing.msToString(ttl)
-                );
-            } else{
-                myString.add(StringUtils.capitalize(s.toString().toLowerCase(Locale.ROOT))
-                        + " [" + s.getActualLevel() + "]: "
-                        + Utils.addCommaToNum(xpMap.get(s)) + "xp (" + Utils.addCommaToNum(xpHr) + "/hr) " +
-                        "|| TNL: "
-                        + Timing.msToString(ttl)
-                );
-            }
-        }
-        if (Vars.get().usingOverloadPots){
-            myString.add("Overload Timer: " + Timing.msToString(Vars.get().overloadTimer.getRemaining()));
-        }
-        int points = ( Utils.getVarBitValue(Varbits.NMZ_POINTS.getId()) - Const.STARTING_NMZ_POINTS);
-        int pointsHr = (int) (points / timeRanMin);
-        myString.add("NMZ Points Gained: " + (
-                Utils.addCommaToNum(points) + " (" + Utils.addCommaToNum(pointsHr) + "hr)"));
-       /* myString.add("Using Super combat potions? " + Vars.get().usingSuperCombat);
-        myString.add("Using Super prayer potions? " + Vars.get().usingPrayerPots);
-        myString.add("Using Absorptions potions? " + Vars.get().usingAbsorptions);
-        myString.add("Using Overload potions? " + Vars.get().usingOverloadPots);*/
-     /*   myString.add("Experience Gained: " + Utils.addCommaToNum(gainedXp) + "xp (+"
-                + gainedLvl + ") || "
-                + "(" + Utils.addCommaToNum(Vars.get().xpHr) + "/hr)"
-        );
-        myString.add("Time to Next Level (" + (currentLvl + 1) + "): "
-                + Timing.msToString(timeToLevel)
-        );
-
-
-        PaintUtil.createPaint(g, myString.toArray(String[]::new));*/
-    }
 
     @Override
     public void onStart() {
